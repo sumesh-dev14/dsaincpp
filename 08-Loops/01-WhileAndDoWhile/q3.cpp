0f55092037beb425378bae1ab135d1fa5f799e53
sumesh-dev14 <sumesh1614@gmail.com>
2025-07-17 18:08:06 +0530

    New program

---
+++ 08-Loops/01-WhileAndDoWhile/q3.cpp
@@ -0,0 +1,11 @@
+// print all the alphabets (uppeercase) with their ASCII VALUES
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    for(int i=97;i<=122;i++){
+        cout<<(char)i<<" = "<<i<<endl;
+    }
+    return 0;
+}
